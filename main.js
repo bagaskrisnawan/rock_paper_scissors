23ee01e6501b2e8c86b43a367518bd59dad32a44
mozun <249906091+bagaskrisnawan@users.noreply.github.com.>
2026-01-07 00:35:44 +0700

    add 3btn & call 4 element

--- main.js
+++ main.js
@@ -1,43 +1,6 @@
-/**
- * membuat function untuk ambil probability pilihan yg akan dikeluarkan oleh computer
- * membuat function untuk mengambil nilai dari user
- * function untuk mengambil pemenang dari user / computer
- */
+const rock = document.getElementsByClassName(".batu");
+const scissors = document.getElementsByClassName(".gunting")
+const paper = document.getElementsByClassName(".kertas");
+const result = document.getElementsByClassName(".result")
 
 
-function getComputerChoice() {
-    return Math.floor(Math.random() * 3)
-}
-
-function getHumanChoice(input = prompt("Rock/Scissors/Paper").toString()) {
-    switch (input.toLowerCase()) {
-        case "batu":
-            return 0;
-        case "gunting":
-            return 1;
-        case "kertas":
-            return 2;
-        default:
-            return undefined;
-    }
-}
-
-
-function playRound(humanChoice, computerChoice) {
-    let x = humanChoice - computerChoice;
-    switch (x) {
-        case 0:
-            return alert("DRAW");
-        case 1:
-        case -2:
-            return alert("LOSE");
-        case -1:
-        case 2:
-            return alert("WIN");
-        default:
-            return alert("ERROR");
-    }
-    
-}
-
-playRound(getHumanChoice(),getComputerChoice())
