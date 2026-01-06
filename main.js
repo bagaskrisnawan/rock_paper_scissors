f5546333a81364c034c939ef70d3b4277ccf2e61
mozun <249906091+bagaskrisnawan@users.noreply.github.com.>
2026-01-07 02:23:13 +0700

    add eventlistener,3function

--- main.js
+++ main.js
@@ -1,6 +1,60 @@
-const rock = document.getElementsByClassName(".batu");
-const scissors = document.getElementsByClassName(".gunting")
-const paper = document.getElementsByClassName(".kertas");
-const result = document.getElementsByClassName(".result")
+const result = document.querySelector(".result")
+const btn = document.querySelectorAll(".choice")
+
+btn.forEach(btn => {
+    btn.addEventListener("click", () => {
+    const choice = btn.dataset.choice;
+    const comResult = getComputerChoice();
+    const humanResult = getHumanChoice(choice);
+    const vsResult = playRound(humanResult,comResult);
+    console.log(choice);
+    console.log(humanResult);
+    console.log(comResult);
+    console.log(vsResult);
+    })
+});
 
 
+//============FUNCTION==================
+function getHumanChoice(choice) {
+        switch (choice) {
+        case "rock":
+            return 0;
+        case "scissors":
+            return 1;
+        case "paper":
+            return 2;
+        default:
+            return undefined;
+}};
+
+function getComputerChoice() {
+    return Math.floor(Math.random() * 3)
+}
+
+function playRound(human, com) {
+  const names = ["Rock", "Scissors", "Paper"];
+
+  let result;
+  if (human === com) {
+    result = "Draw";
+  } else if (
+    (human === 0 && com === 1) ||
+    (human === 1 && com === 2) ||
+    (human === 2 && com === 0)
+  ) {
+    result = "You Win";
+  } else {
+    result = "You Lose";
+  }
+
+  return {
+    human: names[human],
+    computer: names[com],
+    result
+  };
+}
+
+
+
+//======================================
